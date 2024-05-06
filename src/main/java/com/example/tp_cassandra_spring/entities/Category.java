2038fc588f9e5c080d315fa3046fefc820ee8182
wail-10 <koundiwail02@gmail.com>
2024-05-06 16:57:48 +0000

    add category entity

--- src/main/java/com/example/tp_cassandra_spring/entities/Category.java
+++ src/main/java/com/example/tp_cassandra_spring/entities/Category.java
@@ -14,4 +14,5 @@ public class Category {
     @PrimaryKey
     private UUID id;
     private String name;
+
 }
--- src/main/java/com/example/tp_cassandra_spring/repositories/CategoryRepository.java
+++ src/main/java/com/example/tp_cassandra_spring/repositories/CategoryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.cassandra.repository.CassandraRepository;
 import java.util.UUID;
 
 public interface CategoryRepository extends CassandraRepository<Category, UUID> {
+
 }
